30b1fcf3cc6a335e921702fa1c789fde5cd957ef
AkmalMaulana55 <vylingsmall55@gmail.com>
2025-06-05 17:28:54 +0700

    menambahkan class stack private

---
+++ stack.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node()
+    {
+        next = NULL;
+    }
+};
+
+class Stack
+{
+private:
+    Node *top;
+
+
+};
